95b6a217662c2b4eb756333ce97936490190f916
Volnei Neves <volneinevesfilho@hotmail.com>
2022-10-31 11:48:21 -0300

    Implementing controller and mappers with dto

---
+++ src/main/java/com/volnei/parking/controller/mapper/ParkingMapper.java
@@ -0,0 +1,32 @@
+package com.volnei.parking.controller.mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.volnei.parking.controller.dto.ParkingCreateDTO;
+import com.volnei.parking.controller.dto.ParkingDTO;
+import com.volnei.parking.model.Parking;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ParkingMapper {
+
+    private static final ModelMapper MODEL_MAPPER = new ModelMapper();
+
+    public ParkingDTO toParkingDTO(Parking parking) {
+        return MODEL_MAPPER.map(parking, ParkingDTO.class);
+    }
+
+    public List<ParkingDTO> toParkingDTOList(List<Parking> parkingList) {
+        return parkingList.stream().map(this::toParkingDTO).collect(Collectors.toList());
+    }
+
+    public Parking toParking(ParkingDTO dto) {
+        return MODEL_MAPPER.map(dto, Parking.class);
+    }
+
+    public Parking toParkingCreate(ParkingCreateDTO dto) {
+        return MODEL_MAPPER.map(dto, Parking.class);
+    }
+}
